224a226c78a973be11154fb0a96aa134c5bd93c9
DONGSUP KIM <kdongxie@naver.com>
2019-06-07 19:58:59 +0900

    chat

--- agora/agora/urls.py
+++ agora/agora/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('chat/', include('chat.urls')),
     path('accounts/', include('accounts.urls')),
     path('boards/', include('boards.urls')),
     path('admin/', admin.site.urls),
