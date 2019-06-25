caca153054aa325a26d337f9f6801d3998d993f0
GiSeok <hgsok3056@naver.com>
2019-06-25 10:44:15 +0900

    190625 - ing

--- agora/parsed_data/views.py
+++ agora/parsed_data/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import RawData, NumData, LawData, LawNum, ReguData, ReguNum
 from django.core.paginator import Paginator # paging 을 하기 위해 사용
 from django.contrib.auth.decorators import login_required
-from IPython import embed
+
 
 @login_required()
 def index(request):
@@ -32,47 +32,36 @@ def index(request):
              }
     # embed()
     return render(request, 'parsed_data/index.html', context)
+
 @login_required()
 def GOV(request):
     Raws = RawData.objects.all()
-    Raw_Page_Num = NumData.objects.all()
-    paginator1 = Paginator(Raws, 10)
+    paginator = Paginator(Raws, 10)
     page = request.GET.get('page')
-    Raws_lists = paginator1.get_page(page)
+    Raws_lists = paginator.get_page(page)
 
-    context={'Raws': Raws,
-             'Raws_lists': Raws_lists,
-             'Raw_Page_Num':Raw_Page_Num,
-             }
-    # embed()
+    context = {'Raws': Raws, 'Raws_lists': Raws_lists,}
     return render(request, 'parsed_data/GOV.html', context)
+
+
 @login_required()
 def ASSEM(request):
     Laws = LawData.objects.all()
-    Law_Page_Num = LawNum.objects.all()
-    paginator2 = Paginator(Laws, 10)
+    paginator = Paginator(Laws, 10)
     page = request.GET.get('page')
-    Laws_lists = paginator2.get_page(page)
+    Laws_lists = paginator.get_page(page)
 
-    context={'Laws':Laws,
-             'Laws_lists':Laws_lists,
-             'Law_Page_Num':Law_Page_Num,
-             }
-    # embed()
+    context = {'Laws': Laws, 'Laws_lists': Laws_lists}
     return render(request, 'parsed_data/ASSEM.html', context)
+
 
 @login_required()
 def ASSEM_REGU(request):
     Regus = ReguData.objects.all()
-    Regu_page_Num = ReguNum.objects.all()
-    paginator3 = Paginator(Regus, 10)
+    paginator = Paginator(Regus, 10)
     page = request.GET.get('page')
-    Regus_lists = paginator3.get_page(page)
+    Regus_lists = paginator.get_page(page)
 
-    context={'Regus': Regus ,
-             'Regus_lists': Regus_lists,
-             'Regu_page_Num': Regu_page_Num,
-             }
-    # embed()
+    context = {'Regus': Regus, 'Regus_lists': Regus_lists}
     return render(request, 'parsed_data/ASSEM_REGU.html', context)
 
